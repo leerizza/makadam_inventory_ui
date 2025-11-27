c5b84e9cb389a31b79e88a724a12cff4ffc48a7d
Riza Gumelar <leerizza@gmail.com>
2025-11-27 10:42:54 +0700

    Update API_BASE to production URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE = 'http://127.0.0.1:8000';
+export const API_BASE = 'https://makadaminventory-production.up.railway.app';
 
 export const CHART_COLORS = [
   '#4f46e5', '#059669', '#d97706', 
